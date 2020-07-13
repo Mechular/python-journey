ea3054057eb1430c3f026da937f9b92aef44c7aa
Mechular <mikelevy902@gmail.com>
2020-07-13 01:44:04 -0300

    Create associative dictionary loop.py

---
+++ day 1/associative dictionary loop.py
@@ -0,0 +1,11 @@
+# https://www.learnpython.org/en/Conditions
+
+# associative dictionary
+# for loop
+# conditions
+
+data = {"John": 23, "Rick": 33, "Mary": 63, "Ron": 23, "Joel": 51}
+
+for key in data:
+    if key != "Rick" and data[key] >= 33:
+        print("Your name is " + key + ", and you are " + str(data[key]) + " years old.")
