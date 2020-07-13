6cae4b3dfc6b2b763f027c7b8e37821cde2e56a5
Mechular <mikelevy902@gmail.com>
2020-07-13 01:54:05 -0300

    Associative Dictionary Loop Conditions

--- day 1/associative dictionary loop.py
+++ day 1/associative dictionary loop.py
@@ -7,5 +7,5 @@
 data = {"John": 23, "Rick": 33, "Mary": 63, "Ron": 23, "Joel": 51}
 
 for key in data:
-    if key != "Rick" and data[key] >= 33:
+    if (key != "Rick" and data[key] >= 33) or key in ["John"]:
         print("Your name is " + key + ", and you are " + str(data[key]) + " years old.")
